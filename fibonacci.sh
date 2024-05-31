a08263007f3c303baf4648705b8fbaeb123998cd
maazurrahman <101254165+maazurrahman@users.noreply.github.com>
2024-05-31 09:28:34 +0530

    Update fibonacci.sh

--- fibonacci.sh
+++ fibonacci.sh
@@ -7,21 +7,31 @@ read n
 a=0
 b=1
 
-echo "Fibonacci Series up to $n terms:"
+# Function to check if a number is odd
+is_odd() {
+    if [ $(($1 % 2)) -ne 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
-# Printing the first two terms
-echo -n "$a $b "
+echo "Generating Fibonacci Series up to $n terms and saving odd numbers to output.txt"
 
 # Generating Fibonacci series
-for ((i=3;i<=n;i++))
+for ((i=1;i<=n;i++))
 do
-    # Sum of the last two numbers
+    # Checking if current Fibonacci number is odd
+    if is_odd $a; then
+        echo $a >> output.txt
+    fi
+
+    # Calculating the next Fibonacci number
     c=$((a + b))
-    echo -n "$c "
 
     # Updating the values of a and b for the next iteration
     a=$b
     b=$c
 done
 
-echo ""
+echo "Output saved to output.txt"
