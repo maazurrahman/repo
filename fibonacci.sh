d94a884ca5e3e6cfb703555621ce84d7bcfe432c
maazurrahman <101254165+maazurrahman@users.noreply.github.com>
2024-06-01 21:23:29 +0530

    Update fibonacci.sh

--- fibonacci.sh
+++ fibonacci.sh
@@ -1,38 +1,15 @@
 #!/bin/bash
-
-echo "Enter the number of terms:"
-read n
-n=100
-
-# Initializing first two Fibonacci numbers
-a=0
-b=1
-
-# Function to check if a number is odd
-is_odd() {
-    if [ $(($1 % 2)) -ne 0 ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-echo "Generating Fibonacci Series up to $n terms and saving odd numbers to output.txt"
-
-# Generating Fibonacci series
-for ((i=1;i<=n;i++))
+# Create or clear the output file
+output_file="odd_numbers.txt"
+> $output_file
+# Loop through numbers from 1 to 100
+for ((i=1; i<=100; i++))
 do
-    # Checking if current Fibonacci number is odd
-    if is_odd $a; then
-        echo $a >> output.txt
-    fi
-
-    # Calculating the next Fibonacci number
-    c=$((a + b))
-
-    # Updating the values of a and b for the next iteration
-    a=$b
-    b=$c
+   # Check if the number is even
+   if ((i % 2 != 0))
+   then
+       # Print the even number to the output file
+       echo $i >> $output_file
+   fi
 done
-
-echo "Output saved to output.txt"
+echo "Even numbers from 1 to 100 have been written to $output_file"
